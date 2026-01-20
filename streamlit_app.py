f6943cb1bdc1356944a7c46fd82c4074ef6c7857
ooSVoo <164628083+ooSVoo@users.noreply.github.com>
2026-01-20 13:16:44 +0530

    Update streamlit_app.py
    syntax modification

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
         # st.text(smoothiefroot_response.json())
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), width= 'stretch')
 
 
     #st.write(ingredients_string)
